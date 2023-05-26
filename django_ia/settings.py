887c540e2bf612bb6a128ea7115bd2d8312c83d8
beah <beah.dev@gmail.com>
2023-05-25 20:39:54 -0400

    csrf config

--- django_ia/settings.py
+++ django_ia/settings.py
@@ -133,6 +133,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "webapp/static/"),
 ]
 
+CSRF_TRUSTED_ORIGINS = ["https://djangoai-production.up.railway.app/"]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
