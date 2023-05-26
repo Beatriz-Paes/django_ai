c60cdcd19fe1507b436f7aa37a95b89bdb875a98
beah <beah.dev@gmail.com>
2023-05-25 21:44:01 -0400

    staticfiles

--- django_ia/settings.py
+++ django_ia/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "webapp/static/"),
+    os.path.join(BASE_DIR, "static"),
 ]
 
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
